975a2f4783f95f4e8148b86596ed0a545cff926e
Seth Axen <seth.axen@gmail.com>
2021-08-07 16:26:52 +0200

    Add logpFq

--- src/specialfunctions.jl
+++ src/specialfunctions.jl
@@ -7,3 +7,14 @@ pFq((), (), x) = exp(tr(x)) # ₀F₀
 # see P Koev, A Edelman. The efficient evaluation of the hypergeometric function of a
 # matrix argument. Math. Comp. 75 (2006), 833-846
 # pFq(a::NTuple{p}, b::NTuple{q}, x)
+
+function pFq(()::Tuple{}, (α,)::NTuple{1}, z)
+    x = sqrt(z)
+    return besseli(α - 1, 2x) * gamma(α) * x^(1 - α)
+end
+
+logpFq(a, b, z) = log(pFq(a, b, z))
+function logpFq(()::Tuple{}, (α,)::NTuple{1}, z)
+    x = sqrt(z)
+    return logbesseli(α - 1, 2x) + loggamma(α) + (1 - α) * log(x)
+end
