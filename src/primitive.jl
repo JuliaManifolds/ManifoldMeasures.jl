64893479d92099c9efcfde40abe9fd8746b44e81
Seth Axen <seth.axen@gmail.com>
2021-08-08 01:35:42 +0200

    Remove unused overloads

--- src/primitive.jl
+++ src/primitive.jl
@@ -12,5 +12,3 @@ mass(μ) = MeasureTheory.Exp(-logmass(μ))
 LinearAlgebra.normalize(μ::AbstractMeasure) = mass(μ) * μ
 
 Manifolds.base_manifold(μ::AbstractMeasure) = base_manifold(basemeasure(μ))
-Manifolds.manifold_dimension(μ::AbstractMeasure) = manifold_dimension(base_manifold(μ))
-MeasureTheory.testvalue(μ::PrimitiveMeasure) = μ.point
