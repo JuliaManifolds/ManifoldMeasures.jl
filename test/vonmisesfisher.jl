16a3d6b2e33aa71b753a71f61ddd64beed3618ce
Seth Axen <seth.axen@gmail.com>
2021-08-17 02:03:52 +0200

    Test vMF for Stiefel

--- test/vonmisesfisher.jl
+++ test/vonmisesfisher.jl
@@ -2,7 +2,9 @@ using LinearAlgebra, ManifoldMeasures, Manifolds, MeasureTheory, Random, StatsBa
 using Test
 using Manifolds: ℝ
 
-function test_vMF(d, μ; atol=1e-2, check_logdensity=true, estimate_mode=true)
+function test_vMF(
+    d, μ; check_logdensity=true, estimate_mode=true, max_norm=0.1, ndraws=10_000
+)
     M = base_manifold(d)
     x = rand(normalize(basemeasure(d)))
 
@@ -21,8 +23,7 @@ function test_vMF(d, μ; atol=1e-2, check_logdensity=true, estimate_mode=true)
     @test real(eltype(rand(Float32, d))) <: Float32
 
     if estimate_mode
-        n = 100_000
-        xs = [rand(d) for _ in 1:n]
+        xs = [rand(d) for _ in 1:ndraws]
         @test all(x -> is_point(M, x), xs)
         if d isa VonMises{ℝ}
             z̄ = mean(cis, xs)
@@ -32,7 +33,7 @@ function test_vMF(d, μ; atol=1e-2, check_logdensity=true, estimate_mode=true)
             z̄ = mean(xs)
             μ̂ = project(M, z̄)
         end
-        @test norm(μ̂ - μ) ≤ atol
+        @test norm(μ̂ - μ) ≤ max_norm
     end
 end
 
@@ -41,23 +42,23 @@ end
     @testset "VonMises" begin
         @testset "real (μ, κ)" begin
             @test VonMises(; μ=0.5, κ=1.0) isa VonMisesFisher{Circle{ℝ},(:μ, :κ)}
-            @testset for κ in (1e-2, 10, 1e5)
+            @testset for κ in (0.1, 50, 1e5)
                 μ = rand() * 2π - π
-                test_vMF(VonMises(; μ=μ, κ=κ), μ; estimate_mode=κ == 10)
+                test_vMF(VonMises(; μ=μ, κ=κ), μ; estimate_mode=κ == 50)
             end
         end
         @testset "complex (μ, κ)" begin
             @test VonMises(ℂ; μ=im, κ=1.0) isa VonMisesFisher{Circle{ℂ},(:μ, :κ)}
-            @testset for κ in (1e-1, 50, 1e5)
+            @testset for κ in (0.1, 50, 1e5)
                 μ = sign(randn(ComplexF64))
-                test_vMF(VonMises(ℂ; μ=μ, κ=κ), μ; estimate_mode=κ == 10)
+                test_vMF(VonMises(ℂ; μ=μ, κ=κ), μ; estimate_mode=κ == 50)
             end
         end
         @testset "complex (c,)" begin
             @test VonMises(ℂ; c=3 + 4im) isa VonMisesFisher{Circle{ℂ},(:c,)}
-            @testset for κ in (1e-1, 50, 1e5)
+            @testset for κ in (0.1, 50, 1e5)
                 c = randn(ComplexF64)
-                test_vMF(VonMises(ℂ; c=c), sign(c); estimate_mode=κ == 10)
+                test_vMF(VonMises(ℂ; c=c), sign(c); estimate_mode=κ == 50)
             end
         end
     end
@@ -68,22 +69,61 @@ end
                 T = 𝔽 === ℂ ? ComplexF64 : Float64
                 @test VonMisesFisher(n + 1, 𝔽) === VonMisesFisher(Sphere(n, 𝔽))
                 @testset "(μ, κ)" begin
-                    @testset for κ in (1e-1, 50, 1e5)
+                    @testset for κ in (0.1, 50, 1e5)
                         μ = normalize(randn(T, n + 1))
                         test_vMF(
-                            VonMisesFisher(n + 1, 𝔽; μ=μ, κ=κ), μ; estimate_mode=κ == 10
+                            VonMisesFisher(n + 1, 𝔽; μ=μ, κ=κ), μ; estimate_mode=κ == 50
                         )
                     end
                 end
                 @testset "(c,)" begin
-                    @testset for κ in (1e-1, 50, 1e5)
+                    @testset for κ in (0.1, 50, 1e5)
                         c = randn(T, n + 1)
                         test_vMF(
                             VonMisesFisher(n + 1, 𝔽; c=c),
                             normalize(c);
-                            estimate_mode=κ == 10,
+                            estimate_mode=κ == 50,
                         )
                     end
+                end
+            end
+        end
+
+        @testset "Stiefel" begin
+            @testset "Stiefel($n, $k, $𝔽)" for 𝔽 in (ℝ, ℂ), (n, k) in ((3, 1), (4, 3))
+                T = 𝔽 === ℂ ? ComplexF64 : Float64
+                @test VonMisesFisher(n, k, 𝔽) === VonMisesFisher(Stiefel(n, k, 𝔽))
+                @testset "(F,)" begin
+                    F = 50 * randn(T, n, k)
+                    U, D, V = svd(F)
+                    test_vMF(
+                        VonMisesFisher(n, k, 𝔽; F=F),
+                        U * V';
+                        check_logdensity=false,
+                        estimate_mode=false,
+                    )
+                end
+                @testset "(U,D,V)" begin
+                    U = rand(normalize(Hausdorff(Stiefel(n, k, 𝔽))))
+                    V = rand(normalize(Hausdorff(Stiefel(k, k, 𝔽))))
+                    D = sort(50 .* abs.(rand(k)))
+                    test_vMF(
+                        VonMisesFisher(n, k, 𝔽; U=U, D=D, V=V),
+                        U * V';
+                        check_logdensity=false,
+                    )
+                end
+                @testset "(H,P)" begin
+                    H = rand(normalize(Hausdorff(Stiefel(n, k, 𝔽))))
+                    V = rand(normalize(Hausdorff(Stiefel(k, k, 𝔽))))
+                    D = sort(50 .* abs.(randn(k)))
+                    P = V * Diagonal(D) * V'
+                    test_vMF(
+                        VonMisesFisher(n, k, 𝔽; H=H, P=P),
+                        H;
+                        check_logdensity=false,
+                        estimate_mode=false,
+                    )
                 end
             end
         end
