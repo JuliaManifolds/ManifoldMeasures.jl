8102599a5f3e7d743038ebb23dec75b40f1a0fbe
Seth Axen <seth.axen@gmail.com>
2021-08-22 11:25:07 +0200

    Make arguments more concrete

--- src/specialfunctions.jl
+++ src/specialfunctions.jl
@@ -1,7 +1,9 @@
 logbesseli(ν, x) = log(besselix(ν * one(real(x)), x)) + abs(real(x))
 
 # hypergeometric functions with matrix arguments
-pFq((), (), x) = exp(tr(x)) # ₀F₀
+function pFq end
+
+pFq(::Tuple{}, ::Tuple{}, x) = exp(tr(x)) # ₀F₀
 # TODO: Implement ₀F₁
 # see P Koev, A Edelman. The efficient evaluation of the hypergeometric function of a
 # matrix argument. Math. Comp. 75 (2006), 833-846
